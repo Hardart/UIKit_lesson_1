ab302eda7e7f0dd0aed04d86d9fcafa0de24fb81
Hardart <kaino@bk.ru>
2021-04-06 18:55:24 +0300

    authorization check

--- less_1/ViewController.swift
+++ less_1/ViewController.swift
@@ -9,22 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
-    @IBOutlet weak var login: UITextField!
-    @IBAction func entrance(_ sender: Any) {
-        let myLogin = self.login.text
-        if myLogin != "" {
-            print("Your text is \(myLogin!)")
-        }
-    }
-    
-    
+    @IBOutlet weak var login_input: UITextField!
+    @IBOutlet weak var pass_input: UITextField!
     
     override func viewDidLoad() {
 		super.viewDidLoad()
-		self.login.placeholder = "Hello"
     }
 
-
+    @IBAction func entranceBtn(_ sender: Any) {
+        let myLogin = "hardart"
+        let myPass = "12345"
+        let login = self.login_input.text
+        let pass = self.pass_input.text
+        if login == myLogin,
+           pass == myPass {
+            performSegue(withIdentifier: "toInfo", sender: self)
+        } else if login == ""{
+            self.login_input.text = "Поле Логин не может быть пустым"
+        } else {
+            self.login_input.text = "Логин не верный!"
+        }
+    }
+    
 }
 
