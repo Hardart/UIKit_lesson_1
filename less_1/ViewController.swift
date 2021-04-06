3ea2c3f577c3fa69d83645df938e54be0886435a
Hardart <kaino@bk.ru>
2021-04-06 11:36:38 +0300

    Initial Commit

---
+++ less_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  less_1
+//
+//  Created by Артем Шакиров on 06.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
